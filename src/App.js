84416aff88e281c0a4296a092293f22fa9ecdd71
sanya-shch <shchipkov.sanya@ukr.net>
2019-12-21 00:42:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+import InfiniteLoader from "react-window-infinite-loader";
 
-function App() {
+let items = {};
+let requestCache = {};
+
+const getUrl = (rows, start) => `https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&sort=population&fields=population,accentcity&rows=${rows}&start=${start}&facet=country`;
+
+const Row = ({ index, style }) => {
+  const item = items[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+        {item ? `${item.accentcity}: ${item.population}` : "Loading..."}
+      </div>
   );
-}
+};
+
+const isItemLoaded = ({ index }) => !!items[index];
+
+const loadMoreItems = (visibleStartIndex, visibleStopIndex) => {
+  const key = [visibleStartIndex, visibleStopIndex].join(":");
+  if (requestCache[key]) {
+    return;
+  }
+
+  const length = visibleStopIndex - visibleStartIndex;
+  const visibleRange = [...Array(length).keys()].map(
+      x => x + visibleStartIndex
+  );
+  const itemsRetrieved = visibleRange.every(index => !!items[index]);
+
+  if (itemsRetrieved) {
+    requestCache[key] = key;
+    return;
+  }
+
+  return fetch(getUrl(length, visibleStartIndex))
+      .then(response => response.json())
+      .then(data => {
+        data.records.forEach((city, index) => {
+          items[index + visibleStartIndex] = city.fields
+        });
+      })
+      .catch(error => console.error("Error:", error));
+};
+
+const App = () => (
+    <AutoSizer>
+      {({ height, width }) => (
+          <InfiniteLoader
+              isItemLoaded={isItemLoaded}
+              loadMoreItems={loadMoreItems}
+              itemCount={300000}
+          >
+            {({ onItemsRendered, ref }) => (
+                <List
+                    className="List"
+                    height={height}
+                    itemCount={300000}
+                    itemSize={35}
+                    width={width}
+                    ref={ref}
+                    onItemsRendered={onItemsRendered}
+                >
+                  {Row}
+                </List>
+            )}
+          </InfiniteLoader>
+      )}
+    </AutoSizer>
+);
 
 export default App;
